141ba5215c11149a0e55acfff6f59876d82f105f
csv610 <csv610@gmail.com>
2024-12-19 09:06:06 +0530

    add task list

--- task_list.py
+++ task_list.py
@@ -1,5 +1,34 @@
+import logging
 
 class Tasks:
-    TASK_GENERATE_ANSWER = "generate_answer"
-    TASK_SAVE_QUESTION = "save_question"
-    VALID_TASKS = frozenset({TASK_GENERATE_ANSWER, TASK_SAVE_QUESTION})
+    GENERATE_ANSWERS = "generate_answers"
+    SAVE_QUESTIONS   = "save_questions"
+    VALID_TASKS = frozenset({GENERATE_ANSWERS, SAVE_QUESTIONS})
+
+def generate_answer(obj, model_input):
+    model = obj.get_model()
+    if model is None:
+       logging.error("Invalid model selection or initialization failed")  
+       return "Invalid model selection or initialization failed"
+    
+    response = model.get_response(model_input)
+    return response
+
+def execute_task(obj, row):
+    task  = obj.get_assigned_task()
+    dname = obj.get_dataset_name()
+
+    model_input = obj.extract_data(row)
+    if model_input is None:
+       error_msg = "Failed to extract data from the row"
+       logging.error(error_msg)
+       return error_msg
+
+    if task == Tasks.GENERATE_ANSWERS:
+       return generate_answer(obj, model_input)
+        
+    if task == Tasks.SAVE_QUESTIONS:
+       model_input['answer'] = obj.get_correct_answer(row)
+       return model_input
+
+    return "Unknown task"
